3c4c7427cf5bbc27a20a2a14155b48ad6974709d
krystiano13 <crystianotv@gmail.com>
2024-12-03 17:22:26 +0100

    implement hamburger menu

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,19 @@
 import './styles/app.css';
 
+let mobileNavShown = false;
 const mobileNav = document.querySelector("#mobile-nav");
+const hamburgerButton = document.querySelector("#hamburger");
+
+function checkNav() {
+    if(mobileNavShown) {
+        mobileNav.style.transform = "translateY(0vh)";
+    } else {
+        mobileNav.style.transform = "translateY(-100vh)";
+    }
+}
+
+checkNav();
+hamburgerButton.addEventListener("click", () => {
+    mobileNavShown = !mobileNavShown;
+    checkNav();
+});
