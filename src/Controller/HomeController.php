2ddc2d003e80c7ac19b812fe5d4f833faa3c6b79
krystiano13 <crystianotv@gmail.com>
2024-12-03 16:07:37 +0100

    add asset mapper library

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +12,22 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/')]
-    public function index(): Response
+    public function index(BookRepository $bookRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $bookRepository->count();
+        return $this->render('home/index.html.twig', ["count" => $bookRepository->count()]);
+    }
+
+    #[Route('/books/{id}', name: 'book', methods: ['GET'])]
+    public function show(int $id, BookRepository $bookRepository): Response
+    {
+        $book = $bookRepository->find($id);
+
+        if(!$book)
+        {
+            return $this->json(["errors" => ["book not found"]], Response::HTTP_NOT_FOUND);
+        }
+
+        return $this->json(["book" => $book],200);
     }
 }
