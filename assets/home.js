8004898f717d153054cb7d56e29452f1208168b8
krystiano13 <crystianotv@gmail.com>
2024-12-06 19:53:32 +0100

    create js code that renders list of books

--- assets/home.js
+++ assets/home.js
@@ -1,3 +1,5 @@
+const booksList = document.querySelector("#books-list");
+
 const books = await fetch(`http://localhost:8000/book`, {
     method: "GET",
     headers: {
@@ -7,4 +9,12 @@ const books = await fetch(`http://localhost:8000/book`, {
 
 const booksData = await books.json();
 
-console.log(booksData);
+if(booksData.books.length > 0) {
+    booksList.innerHTML = "";
+
+    booksData.books.forEach(item => {
+        const listElement = document.createElement("li");
+        listElement.innerText = `${item.title} - ${item.author}`;
+        booksList.appendChild(listElement);
+    })
+}
