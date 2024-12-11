68d9f9fad0f039547138dbbb743c2a6c553acbd5
krystiano13 <crystianotv@gmail.com>
2024-12-11 16:36:35 +0100

    implement hiding and showing menu's account options based on authentication state

--- assets/menu.js
+++ assets/menu.js
@@ -1,3 +1,5 @@
+const loginButton = document.querySelector("#login");
+const registerButton = document.querySelector("#register");
 const logoutButton = document.querySelector("#logout");
 const logo = document.querySelector("#logo");
 
@@ -7,5 +9,16 @@ function logout() {
     window.location.href = "/login";
 }
 
+if(localStorage.getItem("token")) {
+    registerButton.style.display = "none";
+    loginButton.style.display = "none";
+}
+
+if(!localStorage.getItem("token")) {
+    logoutButton.style.display = "none";
+}
+
 logoutButton.addEventListener("click", () => logout());
+loginButton.addEventListener("click", () => window.location.href = "/login");
+registerButton.addEventListener("click", () => window.location.href = "/register");
 logo.addEventListener("click", () => window.location.href = "/");
