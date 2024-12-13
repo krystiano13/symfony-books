4cf6d64c554888bd0d68c42c46a4ca65ea5056b0
krystiano13 <crystianotv@gmail.com>
2024-12-13 19:46:49 +0100

    implement book uniqueness test

--- tests/BookTest.php
+++ tests/BookTest.php
@@ -8,6 +8,38 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class BookTest extends WebTestCase
 {
+    public function testBookUniqueness()
+    {
+        $client = static::createClient();
+        $entityManager = $client->getContainer()->get('doctrine')->getManager();
+        $faker = \Faker\Factory::create();
+        $bookRepository = $entityManager->getRepository(Book::class);
+
+        $schemaTool = new \Doctrine\ORM\Tools\SchemaTool($entityManager);
+        $metadata = $entityManager->getMetadataFactory()->getAllMetadata();
+
+        $schemaTool->dropSchema($metadata);
+        $schemaTool->createSchema($metadata);
+
+        $book = new Book();
+        $book->setTitle("123");
+        $book->setAuthor("Test Author");
+        $book->setReleaseDate($faker->numberBetween(1900, 2024));
+        $book->setIsbn($faker->isbn10());
+
+        $entityManager->persist($book);
+
+        $entityManager->flush();
+        $crawler = $client->jsonRequest('POST', '/api/books', [
+            'title' => '123',
+            'author' => "Test Author",
+            'release_date' => 2024,
+            'isbn' => $faker->isbn10(),
+        ]);
+
+        $this->assertResponseStatusCodeSame(400);
+    }
+
     public function testBooksAuthorLimit() {
         $client = static::createClient();
         $entityManager = $client->getContainer()->get('doctrine')->getManager();
