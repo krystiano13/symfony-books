3c914eb93ad4c104e2b19096b7b49186b8ebc92b
krystiano13 <crystianotv@gmail.com>
2024-12-13 19:10:58 +0100

    write test for checking books limit

---
+++ tests/BookTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Book;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BookTest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        $client = self::createClient();
+        $entityManager = $client->getContainer()->get('doctrine')->getManager();
+        $faker = \Faker\Factory::create();
+        $bookRepository = $entityManager->getRepository(Book::class);
+
+        $schemaTool = new \Doctrine\ORM\Tools\SchemaTool($entityManager);
+        $metadata = $entityManager->getMetadataFactory()->getAllMetadata();
+
+        $schemaTool->dropSchema($metadata);
+        $schemaTool->createSchema($metadata);
+
+        for ($i = 0; $i < 100; $i++) {
+            $book = new Book();
+            $book->setTitle($faker->sentence(3));
+            $book->setAuthor($faker->name());
+            $book->setReleaseDate($faker->numberBetween(1900, 2024));
+            $book->setIsbn($faker->isbn10());
+
+            $entityManager->persist($book);
+        }
+
+        $entityManager->flush();
+        $crawler = $client->jsonRequest('POST', '/api/books', [
+            'title' => 'New Test Book',
+            'author' => 'Test Author',
+            'release_date' => 2024,
+            'isbn' => $faker->isbn10(),
+        ]);
+
+        $this->assertResponseStatusCodeSame(400);
+    }
+}
