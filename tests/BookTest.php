3f803536f014488fa5dcda27a0a291fcbebbc792
krystiano13 <crystianotv@gmail.com>
2024-12-13 19:22:07 +0100

    implement testing 5 books limit

--- tests/BookTest.php
+++ tests/BookTest.php
@@ -8,7 +8,39 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class BookTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testBooksAuthorLimit() {
+        $client = static::createClient();
+        $entityManager = $client->getContainer()->get('doctrine')->getManager();
+        $faker = \Faker\Factory::create();
+        $bookRepository = $entityManager->getRepository(Book::class);
+
+        $schemaTool = new \Doctrine\ORM\Tools\SchemaTool($entityManager);
+        $metadata = $entityManager->getMetadataFactory()->getAllMetadata();
+
+        $schemaTool->dropSchema($metadata);
+        $schemaTool->createSchema($metadata);
+
+        for ($i = 0; $i < 6; $i++) {
+            $book = new Book();
+            $book->setTitle("123");
+            $book->setAuthor("Test Author");
+            $book->setReleaseDate($faker->numberBetween(1900, 2024));
+            $book->setIsbn($faker->isbn10());
+
+            $entityManager->persist($book);
+        }
+
+        $entityManager->flush();
+        $crawler = $client->jsonRequest('POST', '/api/books', [
+            'title' => 'New Test Book',
+            'author' => "Test Author",
+            'release_date' => 2024,
+            'isbn' => $faker->isbn10(),
+        ]);
+
+        $this->assertResponseStatusCodeSame(400);
+    }
+    public function testBooksLimit(): void
     {
         $client = self::createClient();
         $entityManager = $client->getContainer()->get('doctrine')->getManager();
